932cc1cf7fb900809795fe2fc83f5f1f7bb22517
Sam <holymp2006@gmail.com>
2022-08-05 19:51:12 +0100

    fix: Add swagger

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -9,6 +9,13 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="Pet Store API",
+ *    version="1.0.0",
+ * )
+ * @OA\PathItem(path="/api")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
